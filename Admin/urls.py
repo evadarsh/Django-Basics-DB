dd7459ba182021fb7029e220855ca33f23d880cb
Adarsh <adarshev2002@gmail.com>
2023-12-11 12:09:44 +0530

    Place,category,subcategory DB connections

--- Admin/urls.py
+++ Admin/urls.py
@@ -4,4 +4,7 @@ app_name = 'webadmin'
 urlpatterns = [
     path('district/',views.district,name="district"),
     path('branch/',views.branch,name="branch"),
+    path('place/',views.place,name="place"),
+    path('category/',views.category,name="category"),
+    path('subcategory/',views.subcategory,name="subcategory"),
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -10,17 +10,65 @@ app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def district(request):
+    dis = db.collection("tbl_district").stream()
+    dis_data = []
+    for i in dis:
+        data = i.to_dict()
+        dis_data.append({"district":data,"id":i.id})
+    # print(dis_data)
     if request.method == "POST":
         data = {"district_name":request.POST.get("txt_district")}
         db.collection("tbl_district").add(data)
         return redirect("webadmin:district")
     else:
-        return render(request,"Admin/District.html")
+        return render(request,"Admin/District.html",{"dis":dis_data})
+    
+def place(request):
+    plc = db.collection("tbl_place").stream()
+    plc_data = []
+    for i in plc:
+        data = i.to_dict()
+        plc_data.append({"place":data,"id":i.id})
+    if request.method == "POST":
+        data = {"place_name":request.POST.get("txt_place")}
+        db.collection("tbl_place").add(data)
+        return redirect("webadmin:place")
+    else:
+        return render(request,"Admin/Place.html",{"plc":plc_data})
     
 def branch(request):
+    brn = db.collection("tbl_branch").stream()
+    brn_data = []
+    for i in brn:
+        data = i.to_dict()
+        brn_data.append({"branch":data,"id":i.id})
     if request.method == "POST":
         data = {"branch_name":request.POST.get("txt_branch")}
         db.collection("tbl_branch").add(data)
         return redirect("webadmin:branch")
     else:
-        return render(request,"Admin/Branch.html")
+        return render(request,"Admin/Branch.html",{"brn":brn_data})
+def category(request):
+    cat = db.collection("tbl_category").stream()
+    cat_data = []
+    for i in cat:
+        data = i.to_dict()
+        cat_data.append({"category":data,"id":i.id})
+    if request.method == "POST":
+        data = {"category_name":request.POST.get("txt_category")}
+        db.collection("tbl_category").add(data)
+        return redirect("webadmin:category")
+    else:
+        return render(request,"Admin/Category.html",{"cat":cat_data})
+def subcategory(request):
+    subcat = db.collection("tbl_category").stream()
+    subcat_data = []
+    for i in subcat:
+        data = i.to_dict()
+        subcat_data.append({"subcat":data,"id":i.id})
+    if request.method == "POST":
+        data = {"subcategory_name":request.POST.get("txt_subcategory"),"category_id":request.POST.get("sel_category")}
+        db.collection("tbl_subcategory").add(data)
+        return redirect("webadmin:subcategory")
+    else:
+        return render(request,"Admin/SubCategory.html",{"subcat":subcat_data})
